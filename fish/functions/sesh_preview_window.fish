c51a5c1e3ceba402e3edf39d303310f3e29d013a
Aryk L Anderson <aryk.anderson@nordstrom.com>
2024-07-03 07:51:39 -0700

    Add color support to ls preview window mode

--- fish/functions/sesh_preview_window.fish
+++ fish/functions/sesh_preview_window.fish
@@ -3,7 +3,7 @@ function sesh_preview_window
     set session_id (tmux ls -F '#{session_id}' -f "#{==:#{session_name},$session_name}")
     if test -z $session_id
         set escaped_name (echo $session_name | sed s^~^$HOME^g)
-        eza -lg --tree --level=2 --icons "$escaped_name"
+        eza -lg --tree --level=2 --icons --color=always "$escaped_name"
     else
         tmux capture-pane -ep -t "$session_id"
     end
