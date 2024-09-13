aa21ff4f990b417575f7c86dbeb1c4fdb2d2ec4a
Aryk Anderson <tmp-a-anderson@mercari.com>
2024-09-14 08:28:32 +0900

    Add fish-pet fisher

--- fish/conf.d/fzf.fish
+++ fish/conf.d/fzf.fish
@@ -1,4 +1,3 @@
-# TODO:  
 # fzf.fish is only meant to be used in interactive mode. If not in interactive mode and not in CI, skip the config to speed up shell startup
 if not status is-interactive && test "$CI" != true
     exit
