02a1b8ef0b9c44d608a99b20ec94735f597b23a1
Aryk Anderson <aryk.anderson@gmail.com>
2024-09-14 14:16:37 +0900

    Add figlet

--- fish/functions/sesh_preview_window.fish
+++ fish/functions/sesh_preview_window.fish
@@ -4,14 +4,17 @@ function sesh_preview_window
     else
         set session_name $argv[1]
     end
-    figlet $session_name
     set session_id (tmux ls -F '#{session_id}' -f "#{==:#{session_name},$session_name}")
     if test -z $session_id
         set escaped_name (echo $session_name | sed s^~^$HOME^g)
         if test -d $escaped_name
+            figlet -f doom $session_name
             eza -lg --tree --level=2 --icons --color=always "$escaped_name"
+        else
+            figlet -f isometric2 $session_name
         end
     else
+        figlet -f doom $session_name
         tmux capture-pane -ep -t "$session_id"
     end
 end
