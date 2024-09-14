770106d17a785febe64c8f2166680f3d6b13d478
Aryk Anderson <aryk.anderson@gmail.com>
2024-09-14 14:08:03 +0900

    Fix session kill binding, add some default sessions

--- fish/functions/sesh_list_sessions.fish
+++ fish/functions/sesh_list_sessions.fish
@@ -1,13 +1,13 @@
 function sesh_list_sessions
     sesh list -i | fzf-tmux -p 85%,80% \
-        --no-sort --ansi --border-label ' Pick Session ' --prompt '   ' \
+        --no-sort --ansi --border-label ' Pick Session ' --prompt '  ' \
         --header '[a]ll | [t]mux | confi[g]s | zo[x]ide | tmux-[k]ill | [f]ind' \
         --bind 'ctrl-n:down,ctrl-p:up' \
-        --bind 'ctrl-a:change-prompt(   )+reload(sesh list)' \
-        --bind 'ctrl-t:change-prompt(   )+reload(sesh list -t)' \
-        --bind 'ctrl-g:change-prompt(   )+reload(sesh list -c)' \
-        --bind 'ctrl-x:change-prompt(󱞊   )+reload(sesh list -z)' \
+        --bind 'ctrl-a:change-prompt(   )+reload(sesh list -i)' \
+        --bind 'ctrl-t:change-prompt(   )+reload(sesh list -i -t)' \
+        --bind 'ctrl-g:change-prompt(   )+reload(sesh list -i -c)' \
+        --bind 'ctrl-x:change-prompt(󱞊   )+reload(sesh list -i -z)' \
         --bind 'ctrl-f:change-prompt(   )+reload(fd -H -d 2 -t d -E .Trash . ~)' \
-        --bind 'ctrl-k:execute(tmux kill-session -t {})+change-prompt(   )+reload(sesh list)' \
+        --bind 'ctrl-k:execute(echo {} | awk \'{print $2}\' | xargs -I {} tmux kill-session -t {})+reload(sesh list -i)+change-prompt(   )' \
         --preview 'sesh_preview_window {}'
 end
--- fish/functions/sesh_preview_window.fish
+++ fish/functions/sesh_preview_window.fish
@@ -4,11 +4,13 @@ function sesh_preview_window
     else
         set session_name $argv[1]
     end
-    echo $session_name
+    figlet $session_name
     set session_id (tmux ls -F '#{session_id}' -f "#{==:#{session_name},$session_name}")
     if test -z $session_id
         set escaped_name (echo $session_name | sed s^~^$HOME^g)
-        eza -lg --tree --level=2 --icons --color=always "$escaped_name"
+        if test -d $escaped_name
+            eza -lg --tree --level=2 --icons --color=always "$escaped_name"
+        end
     else
         tmux capture-pane -ep -t "$session_id"
     end
