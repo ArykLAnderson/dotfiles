f40065ed4f989ec375f713f5efa9bf28540bfb20
Aryk Anderson <tmp-a-anderson@mercari.com>
2025-04-16 09:02:14 +0900

    Fix blink config

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -8,6 +8,26 @@ return {
     opts.appearance.kind_icons = vim.tbl_extend("keep", {
       Color = "██", -- Use block instead of icon for color items to make swatches more usable
     }, LazyVim.config.icons.kinds)
+    opts.sources = {
+      -- Add 'avante' to the list
+      default = { "avante", "copilot", "lsp", "path", "buffer" },
+      providers = {
+        avante = {
+          module = "blink-cmp-avante",
+          name = "Avante",
+          opts = {
+            -- options for blink-cmp-avante
+          },
+        },
+        copilot = {
+          name = "copilot",
+          module = "blink-cmp-copilot",
+          kind = "Copilot",
+          score_offset = 100,
+          async = true,
+        },
+      },
+    }
     opts.keymap = {
       ["<C-y>"] = { "select_and_accept" },
       ["<C-h>"] = { "hide" },
